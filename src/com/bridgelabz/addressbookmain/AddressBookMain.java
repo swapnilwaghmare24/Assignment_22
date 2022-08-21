3cea7d391eddcbfc7df4eae67664c8e4c7b1cb67
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-08-21 14:46:22 +0530

    welcome message display

---
+++ src/com/bridgelabz/addressbookmain/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookmain;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book program");
+    }
+}
